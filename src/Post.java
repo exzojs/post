07bf5523fb073a70a43110ec4e7c18736bb4a32d
Серков Евгений <exzojs@gmail.com>
2022-08-08 18:09:49 +0300

    posts

---
+++ src/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String pasport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
